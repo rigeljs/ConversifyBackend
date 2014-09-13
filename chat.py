1ea955dc69f403a90a92caedf697053e645fe6b4
rigeljs <rigeljs@gmail.com>
2014-09-13 15:25:27 -0400

    hopefully this fixes the json

--- chat.py
+++ chat.py
@@ -80,7 +80,7 @@ def update(ws):
             message_json = json.loads(message)
             result = translateAndFetch(message_json)
             print ",".join(result)
-            ws.send("['" + "','".join(result) + "']")
+            ws.send('{"results": ["' + '","'.join(result) + '""]}')
 
 
 def translateAndFetch(request):
