e639890f37f88cdd216ac988851af843803f8422
nikkil331 <limni@seas.upenn.edu>
2014-09-13 13:01:36 -0400

    try fixing database url again?

--- db_connection.py
+++ db_connection.py
@@ -4,7 +4,7 @@ import urlparse
 
 DATABASE_URL = 'postgres://nurpkzhfspjxpx:xdQNuEeCnGu0siyeSEvUGEU7fp@ec2-54-225-255-208.compute-1.amazonaws.com:5432/d9ivq0hggrk4ip'
 urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ[DATABASE_URL])
+url = urlparse.urlparse(DATABASE_URL)
 
 def ConnectToDB():
 	return psycopg2.connect(database=url.path[1:],
