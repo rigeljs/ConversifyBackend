59ec8be9d36290cbd4b86acaf1462bebb769b342
nikkil331 <limni@seas.upenn.edu>
2014-09-13 19:49:10 -0400

    remove d from groupd_id

--- group_dao.py
+++ group_dao.py
@@ -17,7 +17,7 @@ def removeGroup(group_id):
 
 def setGroupName(group_id, name):
 	cur = db_connection.GetCursorForConnection(connection)
-	cur.execute("""UPDATE Groups SET group_name=\'%s\' WHERE groupd_id = %s;""" % (name, groupd_id))
+	cur.execute("""UPDATE Groups SET group_name=\'%s\' WHERE group_id = %s;""" % (name, group_id))
 	connection.commit()
 	cur.close()
 
@@ -27,33 +27,33 @@ def getGroupName(group_id):
 
 def removeUserFromGroup(user_id, group_id):
 	cur = db_connection.GetCursorForConnection(connection)
-	cur.execute("""DELETE FROM UsersToGroups WHERE user_id = %s AND groupd_id = %s""" % (user_id, groupd_id))
+	cur.execute("""DELETE FROM UsersToGroups WHERE user_id = %s AND group_id = %s""" % (user_id, group_id))
 	connection.commit()
 	cur.close()
 
-def addUserToGroup(user_id, groupd_id):
+def addUserToGroup(user_id, group_id):
 	cur = db_connection.GetCursorForConnection(connection)
-	cur.execute("""SELECT user_id FROM UsersToGroups WHERE user_id = %s AND groupd_id = %s;""" % (user_id, groupd_id))
+	cur.execute("""SELECT user_id FROM UsersToGroups WHERE user_id = %s AND group_id = %s;""" % (user_id, group_id))
 	if not cur.fetchone():
-		cur.execute("""INSERT INTO UsersToGroups (user_id, groupd_id, is_admin) VALUES (%s, %s, FALSE);""" % (user_id, groupd_id))
+		cur.execute("""INSERT INTO UsersToGroups (user_id, group_id, is_admin) VALUES (%s, %s, FALSE);""" % (user_id, group_id))
 	connection.commit()
 	cur.close()
 
 def setAdmin(group_id, user_id):
 	cur = db_connection.GetCursorForConnection(connection)
-	cur.execute("""SELECT user_id FROM UsersToGroups WHERE user_id = %s AND groupd_id = %s;""" % (user_id, groupd_id))
+	cur.execute("""SELECT user_id FROM UsersToGroups WHERE user_id = %s AND group_id = %s;""" % (user_id, group_id))
 	if cur.fetchone():
-		cur.execute("""UPDATE UsersToGroups SET is_admin=TRUE WHERE user_id = %s AND groupd_id = %s;""" % (user_id, groupd_id))
+		cur.execute("""UPDATE UsersToGroups SET is_admin=TRUE WHERE user_id = %s AND group_id = %s;""" % (user_id, group_id))
 	else:
-		cur.execute("""INSERT INTO UsersToGroups (user_id, groupd_id, is_admin) VALUES (%s, %s, TRUE);""" % (user_id, groupd_id))
+		cur.execute("""INSERT INTO UsersToGroups (user_id, group_id, is_admin) VALUES (%s, %s, TRUE);""" % (user_id, group_id))
 	connection.commit()
 	cur.close()
 
 def removeAdmin(group_id, user_id):
 	cur = db_connection.GetCursorForConnection(connection)
-	cur.execute("""SELECT user_id FROM UsersToGroups WHERE user_id = %s AND groupd_id = %s AND is_admin = TRUE;""" % (user_id, groupd_id))
+	cur.execute("""SELECT user_id FROM UsersToGroups WHERE user_id = %s AND group_id = %s AND is_admin = TRUE;""" % (user_id, group_id))
 	if cur.fetchone():
-		cur.execute("""UPDATE UsersToGroups SET is_admin=FALSE WHERE user_id = %s AND groupd_id = %s;""" % (user_id, groupd_id))
+		cur.execute("""UPDATE UsersToGroups SET is_admin=FALSE WHERE user_id = %s AND group_id = %s;""" % (user_id, group_id))
 	connection.commit()
 	cur.close()
 
@@ -73,14 +73,14 @@ def getGroupsForUser(user_id):
 		groups.append(record[0])
 	return groups
 
-def userIsAdminForGroup(user_id, groupd_id):
+def userIsAdminForGroup(user_id, group_id):
 	cur = db_connection.GetCursorForConnection(connection)
-	cur.execute("""SELECT is_admin FROM UsersToGroups WHERE user_id = %s AND groupd_id = %s;""" % (user_id, groupd_id))
+	cur.execute("""SELECT is_admin FROM UsersToGroups WHERE user_id = %s AND group_id = %s;""" % (user_id, group_id))
 	return cur.fetchone()[0]
 
 def getAdminForGroup(group_id):
 	cur = db_connection.GetCursorForConnection(connection)
-	cur.execute("""SELECT user_id FROM UsersToGroups WHERE groupd_id = %s AND is_admin = TRUE;""" % (groupd_id))
+	cur.execute("""SELECT user_id FROM UsersToGroups WHERE group_id = %s AND is_admin = TRUE;""" % (group_id))
 	user_ids = []
 	for record in cur:
 		user_ids.append(record[0])
