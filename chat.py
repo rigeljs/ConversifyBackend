ffa69b3b2b54614c6581f3ce0e9ae5e05675ebbe
rigeljs <rigeljs@gmail.com>
2014-09-14 01:17:06 -0400

    remove string casting

--- chat.py
+++ chat.py
@@ -79,8 +79,8 @@ def executeSendMessage(message):
                                                 message["conversation_id"],
                                                 message["sender_id"])
     message = messages_dao.getMessageById(mid)[0]
-    message_to_return = str({"message_id": str(message[0]), "conversation_id": str(message[1]), "group_id": str(message[2]),\
-                             "sender_id": str(message[3]), "content": message[4], "time_updated": message[5]})
+    message_to_return = str({"message_id": message[0], "conversation_id": message[1], "group_id": message[2],\
+                             "sender_id": message[3], "content": message[4], "time_updated": message[5]})
     return (message_to_return, conversation_dao.getUsersOptedInToConversation(str(message[1])))
 
 @sockets.route('/update')
