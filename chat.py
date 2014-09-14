77a8ceb5a6e0a9d4a35fc4bd3115b55cff5c0a6c
nikkil331 <limni@seas.upenn.edu>
2014-09-13 23:00:00 -0400

    cast cid to string

--- chat.py
+++ chat.py
@@ -245,7 +245,7 @@ def createConversation(group_id, topic_name, is_announcement):
                     conversation_dao.addUserToConversation(user_id, cid, False)
             else:
                 conversation_dao.addUserToConversation(user_id, cid, True)
-        return ["success", int(cid)]
+        return ["success", str(cid)]
     except Exception:
         traceback.print_exc(file=sys.stdout)
         return ["failure"]
