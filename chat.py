ee5bb17a5c489a7a7df010503560fb5a134e4ae6
rigeljs <rigeljs@gmail.com>
2014-09-14 00:36:27 -0400

    debuggging

--- chat.py
+++ chat.py
@@ -75,9 +75,9 @@ def executeSendMessage(message):
                                                 message["conversation_id"],
                                                 message["sender_id"])
     message = messages_dao.getMessageById(mid)[0]
-    message_to_return = str({"message_id": message[0], "conversation_id": message[1], "group_id": message[2],
+    message_to_return = str({"message_id": message[0], "conversation_id": message[1], "group_id": message[2],\
                              "sender_id": message[3], "content": message[4], "time_updated": message[5]}
-    return (message_to_return, conversation_dao.getUsersOptedInToConversation(message["conversation_id"]))
+    return (message_to_return, conversation_dao.getUsersOptedInToConversation(message[1]))
 
 @sockets.route('/update')
 def update(ws):
